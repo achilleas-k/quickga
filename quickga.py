06b5c6fca86adb7cb01dabc0be60f1a01a31fa53
Achilleas <achilleas.k@gmail.com>
2014-08-18 17:55:28 +0300

    Minor changes, comments and code clarification
    - Cutpoint selection rewritten (simpler)
    - A few TODO items added as code comments (not docstring)
    - Double quoted strings

--- quickga.py
+++ quickga.py
@@ -33,7 +33,7 @@ class GA:
 
     def __init__(self, max_population, chromlength,
             mutation_probability=1, mutation_strength=0.1,
-            selection_percentage=0.2, init_pop=None, outputfile='quickga.log'):
+            selection_percentage=0.2, init_pop=None, outputfile="quickga.log"):
         self.max_population = max_population
         self.mutation_probability = mutation_probability
         self.mutation_strength = mutation_strength
@@ -54,37 +54,37 @@ class GA:
             self.population.append(newind)
 
     def fitnessfunc(self):
-        '''
+        """
         Calculates the fitness value of an individual, stores it in the
         individual's `fitness` member and returns it.
         This function is the target function to be optimised by the GA.
 
         This function should be overwritten by any implementation of the class.
-        '''
+        """
 
     def evaluate_population(self, optargs):
-        '''
+        """
         Updates the fitness values of the population, for all individuals
         flagged as unevaluated.
-        '''
+        """
         for ind in self.population:
             if ind.fitness is None:
                 self.fitnessfunc(ind, optargs)
 
     def sort_population(self, optargs):
-        '''
+        """
         Sorts the population based on fitness values such that the first
         individual is the best in the population.
         This method first calls the `evaluate_population` method to calculate
         the fitness values of any unevaluated individuals
-        '''
+        """
         self.evaluate_population(optargs)
         self.population.sort(key=lambda individual: individual.fitness)
 
     def crossover(self, parent_one, parent_two):
-        '''
+        """
         Two-point crossover.
-        '''
+        """
         parchrom_one = copy.deepcopy(parent_one.chromosome)
         parchrom_two = copy.deepcopy(parent_two.chromosome)
         if parent_one.chromlength != parent_two.chromlength:
@@ -93,13 +93,12 @@ class GA:
 
         cutpoint_one = cutpoint_two = 0
         chromlength = parent_one.chromlength
+        chrom_idx = range(chromlength)
+        np.random.shuffle(chrom_idx)
+        cutpoints = (chrom_idx[0], chrom_idx[1])
+        cutpoint_one = min(cutpoints)
+        cutpoint_two = max(cutpoints)
 
-        while cutpoint_one == cutpoint_two:
-            cutpoint_one = int(np.floor(np.random.random()*(chromlength-1)))
-            cutpoint_two = int(np.floor(np.random.random()*(chromlength-1)))
-
-        cutpoint_one, cutpoint_two = (min(cutpoint_one, cutpoint_two),
-                                        max(cutpoint_one, cutpoint_two))
         childchrom_one = np.append(parchrom_one[0:cutpoint_one],
                             parchrom_two[cutpoint_one:cutpoint_two])
         childchrom_one = np.append(childchrom_one, parchrom_one[cutpoint_two:])
@@ -107,15 +106,14 @@ class GA:
                             parchrom_one[cutpoint_one:cutpoint_two])
         childchrom_two = np.append(childchrom_two, parchrom_two[cutpoint_two:])
         if childchrom_one.size != childchrom_two.size:
-            exit('WTF')
+            exit("ERROR: Child chromosome lengths don't match."
+                 " This shouldn't happen")
         if not (childchrom_one.size == parchrom_one.size ==
                 childchrom_two.size == parchrom_two.size):
-                    exit('ERROR: Chromosome lengths changed during crossover')
+                    exit("ERROR: Chromosome lengths changed during crossover")
 
-        '''
-        Turn childchroms into individuals
-        Childrens' generation will be youngest parent's gen+1
-        '''
+        # Turn childchroms into individuals
+        # Childrens' generation will be youngest parent's gen+1
         last_gen = max(parent_one.generation, parent_two.generation)
         newind_one = self.Individual(childchrom_one, last_gen+1)
         newind_two = self.Individual(childchrom_two, last_gen+1)
@@ -123,9 +121,9 @@ class GA:
         return newind_one, newind_two
 
     def mutate(self, individual):
-        '''
+        """
         Mutates an individual's chromosomes in place.
-        '''
+        """
         try:
             if individual.chromosome.dtype is np.dtype(np.float):
                 individual.chromosome += np.random.normal(
@@ -133,17 +131,17 @@ class GA:
                     individual.chromlength)
             individual.fitness = None # mark fitness as 'unevaluated'
         except (TypeError, AttributeError):
-            exit('ERROR: (Mutation) Invalid chromosome type. '
-                            'Must be numpy float or int array.')
+            exit("ERROR: (Mutation) Invalid chromosome type. "
+                            "Must be numpy float or int array.")
 
     def insert(self, newinds):
-        '''
+        """
         Inserts the new individuals into the population.
         If the population exceeds the max_population limit, the worst
         individuals from the original population is discarded.
 
         Assumes the population is evaluated and sorted.
-        '''
+        """
         newpopsize = len(newinds)+len(self.population)
         overflow = newpopsize - self.max_population
         if overflow <= 0:
@@ -152,10 +150,10 @@ class GA:
             self.population = self.population[:-overflow] + newinds
 
     def select(self, method):
-        '''
+        """
         Currently selects individuals randomly for recombination.
         Later it will implement several methods for other selection methods.
-        '''
+        """
         if method == 'rand':
             r1 = r2 = 0
             popsize = len(self.population)
@@ -170,16 +168,16 @@ class GA:
                   "Falling back to random selection.")
             return self.select('rand')
         else:
-            exit('ERROR: Invalid selection method')
+            exit("ERROR: Invalid selection method")
 
     def saveprogress(self, gen, bestind, alltime_bestind):
         self.outputfile.write(
-                'Generation %i\n'
-                'Best individual of cur gen:\n'
-                '%s\n'
-                'Best individual so far:\n'
-                '%s\n'
-                '---\n' % (gen, bestind, alltime_bestind))
+                "Generation %i\n"
+                "Best individual of cur gen:\n"
+                "%s\n"
+                "Best individual so far:\n"
+                "%s\n"
+                "---\n" % (gen, bestind, alltime_bestind))
 
     def optimise(self, num_generations, *optargs):
         self.sort_population(optargs)
@@ -209,19 +207,19 @@ class GA:
                 bestind = copy.deepcopy(curbest)
             if gen % 100 == 0:
                 self.saveprogress(gen, bestind, alltime_bestind)
-                print('Generation %i\nBest fitness: %f (all time best: %f)\n' % (
+                print("Generation %i\nBest fitness: %f (all time best: %f)\n" % (
                     gen, bestind.fitness, alltime_bestind.fitness))
-            if bestind_age > 4000: #arbitrary age limit
-                '''
-                Recreate entire population, except best individual.
-                '''
-                print('Age limit reached. Re-initializing population\n')
-                self.outputfile.write('Age limit reached. '
-                        'Re-initializing population.\n\n')
+            if bestind_age > 4000: #arbitrary age limit (TODO: Parameterise)
+                # Recreate entire population, except best individual
+                # TODO: Also parameterise number of individuals to keep when
+                # resetting
+                print("Age limit reached. Re-initializing population\n")
+                self.outputfile.write("Age limit reached. "
+                        "Re-initializing population.\n\n")
                 self.init_population(self.max_population, self.chromlength)
 
         self.saveprogress(gen, bestind, alltime_bestind)
-        print('Final generation\nBest individual:\n%s\n' % (
+        print("Final generation\nBest individual:\n%s\n" % (
             bestind))
         self.outputfile.close()
 
@@ -237,16 +235,16 @@ class GA:
             return len(self.chromosome)
 
         def __repr__(self):
-            stringrep = 'Chromosome:\n'
+            stringrep = "Chromosome:\n"
             for c in self.chromosome:
                 stringrep += '%f, ' % c
             stringrep = stringrep[:-2]
-            stringrep += '\nFitness: %f\n' % self.fitness
+            stringrep += "\nFitness: %f\n" % self.fitness
             return stringrep
 
         def __eq__(self, other):
-            '''
+            """
             Equality operator checks chromosomes for all() equality.
-            '''
+            """
             return np.all(self.chromosome == other.chromosome)
 
